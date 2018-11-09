9cdcc9b26bba6a6a7bb005464999213b8fa1c833
adtm <tomas.eglinskas@gmail.com>
2018-11-09 23:56:44 +0200

    Make Page Layout responsive to different screen sizes

--- src/components/Layout/PageContainer.jsx
+++ src/components/Layout/PageContainer.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  padding: 50px 200px;
+  padding: 50px 15%;
+
+  @media (max-width: 425px) {
+    padding: 50px 10%;
+  }
+  @media (max-width: 320px) {
+    padding: 50px 5%;
+  }
 `;
 
 export default ({ children }) => <Container>{children}</Container>;
